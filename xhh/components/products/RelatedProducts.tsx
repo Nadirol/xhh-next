e9068921f0f6b147661ddc3fc23d00e173d406e3
Khanh Duy <khanhduycb1510@gmail.com>
2023-08-22 09:51:46 +0700

    fix bug

--- xhh/components/products/RelatedProducts.tsx
+++ xhh/components/products/RelatedProducts.tsx
@@ -91,7 +91,7 @@ const RelatedProducts = ({ t, product }: { t: TFunction, product: IProduct }) =>
 
             <div className="flex gap-12 items-center overflow-x-scroll scrollbar-hide snap-x pr-4" ref={sliderRef}>
                 {relatedProducts?.map((p, index) => (
-                <ProductCard key={index} title={p.title_vi} image={p.image_url} slug={p.slug}/>
+                  <ProductCard key={index} title={p.title_vi} image={p.image_url} slug={p.slug} category={p.category} t={t}/>
                 ))}
                 {fetchMoreVisible && (
                 <button className="hover:pl-2 transition-[padding]" onClick={handleLoadMore}>
