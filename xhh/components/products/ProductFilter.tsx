366830bd1e3a5bbfe42958aac96a1e2dd5f4a81d
Nadirol <99162550+Nadirol@users.noreply.github.com>
2023-12-28 08:15:39 +0700

    Update ProductFilter.tsx

--- xhh/components/products/ProductFilter.tsx
+++ xhh/components/products/ProductFilter.tsx
@@ -25,14 +25,6 @@ const ProductFilter = ({ t }: { t: TFunction }) => {
                         {t('shelf')}
                     </span>
                 </Link>
-                <Link href={`/${i18n?.language}/products?category=curtain`} className="py-2 relative before:absolute before:z-0 
-                before:bg-red-500 before:w-full before:h-full flex items-center
-                [&:hover]:before:translate-x-0 before:translate-x-[-105%] overflow-hidden
-                before:transition-transform hover:text-neutral-50 transition-[color] duration-75">
-                    <span className="relative z-10 md:px-4">
-                        {t('curtains')}
-                    </span>
-                </Link>
                 <Link href={`/${i18n?.language}/products?category=wooden-tile`} className="py-2 relative before:absolute before:z-0 
                 before:bg-red-500 before:w-full before:h-full flex items-center
                 [&:hover]:before:translate-x-0 before:translate-x-[-105%] overflow-hidden
