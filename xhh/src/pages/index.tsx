be8a4c849c18f44bf5630b95643a603767e9fbeb
Nadirol <99162550+Nadirol@users.noreply.github.com>
2024-01-04 10:49:18 +0700

    Update index.tsx

--- xhh/src/pages/index.tsx
+++ xhh/src/pages/index.tsx
@@ -50,7 +50,6 @@ export default function Home({ data }: { data: IPost[]}) {
           <Category t={t}/>
           {/* <FeaturedItems t={t}/> */}
 
-          <Curtains t={t}/>
           <About t={t}/>
           <Contact t={t}/>
 
